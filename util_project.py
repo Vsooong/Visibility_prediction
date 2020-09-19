10a1e46513e157972f1217793fafe14a3df736cf
vsong <18221188857@163.com>
2020-09-19 19:11:38 +0800

    1.5

--- util_project.py
+++ util_project.py
@@ -2,6 +2,8 @@ import os
 import yaml
 import argparse
 import torch
+from torchvision import transforms
+from PIL import Image
 
 
 def yaml_config_hook(config_file):
@@ -26,6 +28,7 @@ def yaml_config_hook(config_file):
     return cfg
 
 
+
 def get_config():
     parser = argparse.ArgumentParser(description="visibility")
     config = yaml_config_hook("config_vis.yaml")
@@ -42,3 +45,27 @@ def get_config():
     return args
 
 args=get_config()
+import os
+dic_path='F:/data/visibility/output/'
+out_path='F:/data/visibility/train/'
+
+file_path=os.path.join(dic_path,'frame_000001_1100.0.jpg')
+img=Image.open(file_path).convert('RGB')
+special_transform = transforms.Compose([
+    transforms.CenterCrop(size=(260,520)),
+    # transforms.Resize(size=(360,640)),
+    # transforms.RandomResizedCrop(size=(360,640)),
+    # transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ToTensor(),
+])
+# special_transform = transforms.Compose([
+#     transforms.Resize(size=(360,640)),
+#     transforms.RandomResizedCrop(size=(360,640)),
+#     transforms.RandomHorizontalFlip(p=0.5),
+#     transforms.ToTensor(),
+# ])
+pil_transform= transforms.ToPILImage()
+image=special_transform(img)
+print(image.size())
+save_img=pil_transform(image)
+save_img.save(os.path.join(out_path,'1.jpg'))
--- video_process.py
+++ video_process.py
@@ -18,7 +18,7 @@ class VideoDataset(Dataset):
         img_path = os.path.join(args.img_root_dir, self.imgs[idx])
         img = Image.open(img_path).convert("RGB")
         label = self.imgs[idx].split('_')[-1].split('.')[0]
-        label = float(label)/50
+        label = float(label) / 50
         if self.transforms is not None:
             img = self.transforms(img)
         label = torch.as_tensor(label, dtype=torch.float)
@@ -29,32 +29,57 @@ class VideoDataset(Dataset):
 
 
 class VideoSequenceDataset(Dataset):
-    def __init__(self, transforms=None):
+    def __init__(self, transforms=None, interval=60, wsize=10, stride=60):
         self.transforms = transforms
         self.imgs = list(sorted(os.listdir(args.img_root_dir)))
-        self.img_seq, self.lbl_seq = self._build_video_seq()
+        self.interval, self.wsize, self.stride = interval, wsize, stride
+        self.img_seq= self._build_video_seq()
 
     def __getitem__(self, idx):
-
-        return self.img_seq[idx], self.lbl_seq[idx]
+        imgs=self.img_seq[idx]
+        images=[]
+        labels=[]
+        for i in imgs:
+            img_path = os.path.join(args.img_root_dir, i)
+            img = Image.open(img_path).convert("RGB")
+            label = self.imgs[idx].split('_')[-1].split('.')[0]
+            label = float(label) / 50
+            if self.transforms is not None:
+                img = self.transforms(img)
+            images.append(img)
+            labels.append(torch.as_tensor(label))
+        images=torch.stack(images,dim=0)
+        labels=torch.stack(labels,dim=0)
+        return images, labels
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.img_seq)
 
     # x: (b, c, t, h, w)
     def _build_video_seq(self):
         img_data = []
-        label_data = []
-        for idx in range(len(self.imgs)):
-            img_path = os.path.join(args.img_root_dir, self.imgs[idx])
-            img = Image.open(img_path).convert("RGB")
-            label = random.randint(1000, 7000)
-            if self.transforms is not None:
-                img = self.transforms(img)
-            img_data.append(img)
-            label = torch.as_tensor(label, dtype=torch.float)
-            label_data.append(label)
-        return torch.as_tensor(img_data), torch.as_tensor(label_data)
+        start_idx = 0
+        length = len(self.imgs)
+        while start_idx < length:
+            end_idx = min(length, start_idx +self.interval* self.wsize,)
+            excerpt = self.imgs[start_idx:end_idx]
+            start=0
+            record=[]
+            while start + self.interval<=len(excerpt):
+                record.append(excerpt[start])
+                start += self.interval
+            if len(record)==self.wsize:
+                img_data.append(record)
+            start_idx += self.stride
+        return img_data
+        # for idx in range(len(self.imgs)):
+        #     time_stamp = int(self.imgs[idx].split('_')[1])
+        #     img_data.append(time_stamp)
+        #     label = self.imgs[idx].split('_')[-1].split('.')[0]
+        #     label = float(label) / 50
+        #     label = torch.as_tensor(label, dtype=torch.float)
+        #     label_data.append(label)
+        # return torch.as_tensor(img_data), torch.as_tensor(label_data)
 
 
 # t,b,c,h,w
@@ -72,16 +97,20 @@ if __name__ == '__main__':
     import torch
 
     device = args.device
-    dataset = VideoDataset(get_transform(train=False))
+    dataset = VideoSequenceDataset(get_transform(train=False))
     data_loader = torch.utils.data.DataLoader(
         dataset,
-        batch_size=16,
+        batch_size=2,
         shuffle=False,
         num_workers=args.workers,
     )
+    for i,j in data_loader:
+        print(i.size())
+        print(j.size())
+
     # file='F:/data/visibility/data/Fog20200313000026.mp4'
     # pts,video_fps=read_video_timestamps(file)
     # print(pts)
     # print(video_fps)
-    for images, targets in data_loader:
-        print(targets)
+    # for images, targets in data_loader:
+    #     print(targets)
